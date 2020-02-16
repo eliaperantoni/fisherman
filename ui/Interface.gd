96876f8ef59126230b578e308126bd364df47cfa
Frex <nico.fretti@gmail.com>
2020-02-16 21:32:45 +0100

    Set progress bar and label of life and oxygen

--- ui/Interface.gd
+++ ui/Interface.gd
@@ -2,9 +2,24 @@ extends NinePatchRect
 
 var lifeBar
 var oxygenBar
+var lifeLabel
+var oxygenLabel
 
 func _ready():
-	pass
+	lifeBar = $LifeBar/TextureProgress
+	lifeLabel = $LifeBar/Label
+	oxygenBar = $OxygenBar/TextureProgress
+	oxygenLabel  = $OxygenBar/Label
+	_set_oxygen(29)
+	
+	
+func _set_oxygen(value):
+	oxygenBar.value = value
+	oxygenLabel.text = "O2 %s/100" % value
+
+func _set_life(value):
+	lifeBar.value = value
+	lifeLabel.text = "HB %s/100" % value
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):

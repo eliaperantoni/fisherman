84a434a95ecadda0a72b06d939c9c9cdea443c80
Frex <nico.fretti@gmail.com>
2020-02-16 21:16:54 +0100

    [Add]Simple interface lifebar, deep and oxygenbar

---
+++ ui/Interface.gd
@@ -0,0 +1,11 @@
+extends NinePatchRect
+
+var lifeBar
+var oxygenBar
+
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass

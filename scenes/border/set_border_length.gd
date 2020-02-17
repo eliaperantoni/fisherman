e445b5ebf29757476130f142a063df864760dffe
Elia Perantoni <perantonielia0@gmail.com>
2020-02-17 21:53:12 +0100

    Refactoring

--- scenes/border/set_border_length.gd
+++ scenes/border/set_border_length.gd
@@ -1,7 +1,7 @@
-extends Node
+extends StaticBody2D
 
 export var length = 32
+onready var sprite = $Sprite
 
 func _ready():
-	var sprite: Sprite = $Sprite
 	sprite.region_rect = Rect2(0, 0, 32, length)

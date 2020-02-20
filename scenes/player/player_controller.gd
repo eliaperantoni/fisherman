34fc5a138d80b72c16f2679de5d61a027d18df75
Frex <nico.fretti@gmail.com>
2020-02-21 00:52:39 +0100

    Edit bug

--- scenes/player/player_controller.gd
+++ scenes/player/player_controller.gd
@@ -1,13 +1,17 @@
 extends RigidBody2D
 
-export var drag_coefficient = 0.1
-export var impulse_multiplier = 50000
-
-var can_swim = true
-signal player_moved
+export var dragCoefficient = 0.1
+export var impulseMultiplier = 50000
+export var oxygen = 100
+export var life = 100
+export var oxygen_refill = 10
+export var oxygen_decrease = 4
+var canSwim = true
+signal playerMoved
+signal oxygenModified
 
 func _physics_process(delta):
-	self.linear_velocity *= (1 - drag_coefficient)
+	self.linear_velocity *= (1 - dragCoefficient)
 
 	var direction = Vector2(0, 0)
 
@@ -24,18 +28,33 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		direction += Vector2(0, -1)
 	direction = direction.normalized()
-	if not can_swim:
+	if not canSwim:
 		direction.y = 0
-	self.apply_central_impulse(direction * delta * impulse_multiplier * (10 if Input.is_key_pressed(KEY_SHIFT) else 1))
-	if direction != Vector2(0,0) or not can_swim:
-		emit_signal("player_moved", position)
+	self.apply_central_impulse(direction * delta * impulseMultiplier * (10 if Input.is_key_pressed(KEY_SHIFT) else 1))
+	if direction != Vector2(0,0) or not canSwim:
+		emit_signal("playerMoved", position)
+	if(canSwim):
+		oxygen -= delta*oxygen_decrease;
+		_oxygen_modified(oxygen)
+	else:
+		oxygen += delta*oxygen_refill;
+		_oxygen_modified(oxygen)
+
+func _oxygen_modified(value):
+	#Il valore inserito è già in percentuale
+	if(value>100):
+		value=100
+	if(value<0):
+		value=0
+	oxygen = value
+	emit_signal("oxygenModified",int(oxygen))
 
 func _on_water_entered(body):
 	if body == self:
-		can_swim = true
+		canSwim = true
 		self.gravity_scale = 0
 
 func _on_water_exited(body):
 	if body == self:
-		can_swim = false
-		self.gravity_scale = 9.8
+		canSwim = false
+		self.gravity_scale = 10

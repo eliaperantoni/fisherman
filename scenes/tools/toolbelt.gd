598770f086eef1f412783271e8227607003e61ec
Elia Perantoni <perantonielia0@gmail.com>
2020-02-22 00:18:38 +0100

    Moved toolbelt to its own scene

--- scenes/tools/toolbelt.gd
+++ scenes/tools/toolbelt.gd
@@ -1,13 +1,16 @@
-extends Node
+extends Node2D
 
 class_name ToolBelt
+
+signal tool_changed(index)
 
 var current_tool_i = 0 setget set_current_tool_i
 var tools = {
 	1: preload("res://scenes/tools/hand_tools/Flashlight.tscn").instance()
 }
 
-func _init(player):
+func _ready():
+	var player = get_parent()
 	for key in tools:
 		tools[key].player = player
 
@@ -27,6 +30,6 @@ func switch_tool_if_different(new_tool_i):
 		# The "self" here is necessary otherwise the setter won't be triggered
 		self.current_tool_i = new_tool_i
 
-func on_input(event):
-	if event is InputEventKey and event.scancode >= KEY_1 and event.scancode <= KEY_5:
-		switch_tool_if_different(event.scancode-KEY_1)
+func _input(ev):
+	if ev is InputEventKey and ev.scancode >= KEY_1 and ev.scancode <= KEY_5:
+		switch_tool_if_different(ev.scancode-KEY_1)

71d685232a7429e09e1fa5d6fbd1114e0c427804
Elia Perantoni <perantonielia0@gmail.com>
2020-02-28 22:16:58 +0100

    Fish spawn density curve

--- scenes/main/spawner.gd
+++ scenes/main/spawner.gd
@@ -4,20 +4,34 @@ export (Array, PackedScene) var scenes
 
 export var initial_spawn_amount: int
 
+export var spawn_range_from = 10
+export var spawn_range_to = 2700
+
 func _ready():
 	randomize()
 	
-	for i in initial_spawn_amount:
-		_spawn_random_fish(_generate_random_coords())
+	var spawned = 0
+	while spawned < initial_spawn_amount:
+		if try_spawning_random_fish(generate_random_coords()):
+			spawned += 1
 	
-func _spawn_random_fish(at: Vector2):	
+func try_spawning_random_fish(at: Vector2) -> bool:
 	var scene = scenes[randi() % scenes.size()]
-	var instance: Node2D = scene.instance()
+	var instance = scene.instance()
+		
+	if randf() > instance.spawn_curve.interpolate(y_to_0_1_range(at.y)):
+		return false
+	
 	instance.position = at
 	instance.add_to_group("fish")
 	add_child(instance)
 	
-func _generate_random_coords() -> Vector2:
+	return true
+
+func generate_random_coords() -> Vector2:
 	var x = rand_range(-600, 600)
-	var y = rand_range(10, 2700)
+	var y = rand_range(spawn_range_from, spawn_range_to)
 	return Vector2(x, y)
+	
+func y_to_0_1_range(y):
+	return (y - spawn_range_from)/(spawn_range_to - spawn_range_from)
